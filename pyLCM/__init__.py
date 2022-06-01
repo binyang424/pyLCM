a4c32a46b3a3cd690dc168f6ec203813802523fa
Bin <bin.yang@polymtl.ca>
2022-06-01 18:41:22 -0400

    pip package

---
+++ pyLCM/__init__.py
@@ -0,0 +1,9 @@
+"""
+pyLCM.
+
+Flow calculation in reinforcement textiles.
+"""
+
+__version__ = "0.0.1"
+__author__ = 'Bin Yang'
+__credits__ = 'Polytechnique Montreal & Wuhan University of Technology'
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='pyLCM',
+    version='0.0.1',    
+    description='Flow calculation in reinforcement textiles.',
+    url='https://github.com/binyang424/pyLCM',
+    author='Bin Yang',
+    author_email='bin.yang@polymtl.ca',
+    license='BSD 2-clause',
+    packages=['pyLCM'],
+    install_requires=['numpy>=0.5',             
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
